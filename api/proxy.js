82b4afb834e3a5f9bb70456defbc275ba839b394
danggerenba <119066997+danggerenba@users.noreply.github.com>
2024-07-11 04:13:17 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,6 +1,10 @@
 const request = require('request');
 
 export default (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // 添加CORS头
+    res.setHeader('Access-Control-Allow-Methods', 'GET'); // 设置允许的方法
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // 设置允许的请求头
+
     const { key } = req.query;
     const apiUrl = `https://api.1-sms.com/api/sms/get?key=${key}`;
     
